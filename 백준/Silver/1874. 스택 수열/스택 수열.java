4cd188f2860e3c1e0ab7f9ebba1f1a7f51ba626b
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-01-14 21:33:11 +0900

    [Silver II] Title: 스택 수열, Time: 1192 ms, Memory: 41020 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Deque<Integer> deque = new ArrayDeque<>();
+        Stack<String> answer = new Stack<>();
+        int cur = 1;
+        
+        for (int num : arr) {
+            while (cur <= num) {
+                deque.push(cur);
+                answer.push("+");
+                cur++;
+            }
+            if (!deque.isEmpty() && deque.peek() == num) {
+                deque.pop();
+                answer.push("-");
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+        for (String operation : answer) {
+            System.out.println(operation);
+        }
+    }
+}
