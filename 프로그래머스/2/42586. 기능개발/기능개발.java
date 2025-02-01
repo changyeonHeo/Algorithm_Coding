77814514022adf31eed3029d31092596e220cb41
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-02-01 16:44:05 +0900

    [level 2] Title: 기능개발, Time: 0.20 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,17 +4,18 @@ class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> list = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i< progresses.length; i++){
-            if((100- progresses[i]) % speeds[i] == 0){
+        
+        for(int i =0; i < progresses.length;i++){
+            if((100 - progresses[i]) % speeds[i] == 0){
                 q.add((100-progresses[i]) / speeds[i]);
             }else{
-                q.add((100 - progresses[i]) / speeds[i] + 1);
+                q.add((100-progresses[i]) / speeds[i] + 1); 
             }
         }
         int x = q.poll();
         int count = 1;
         while(!q.isEmpty()){
-            if(x>= q.peek()){
+            if(x >= q.peek()){
                 count++;
                 q.poll();
             }else{
@@ -25,7 +26,7 @@ class Solution {
         }
         list.add(count);
         int[] ans = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
+        for(int i =0; i < list.size(); i++){
             ans[i] = list.get(i);
         }
         return ans;
