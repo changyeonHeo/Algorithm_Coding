0c82ca8711a328b70ae01ecdeafc1c536996954e
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-02-07 19:48:35 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 808 ms, Memory: 129408 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,27 +10,29 @@ public class Main{
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        int[][] dp = new int[n+1][n+1];
         int[][] arr = new int[n+1][n+1];
-        for(int i =1; i<=n;i++){
+        int[][] ans = new int[n+1][n+1];
+        
+        for(int i = 1; i<=n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j<=n;j++){
-                dp[i][j] = Integer.parseInt(st.nextToken());
+            for(int j =1; j <=n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i =1; i<=n;i++){
-            for(int j= 1; j<=n;j++){
-                arr[i][j] = arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1] + dp[i][j];
+        
+        for(int i = 1; i <=n; i++){
+            for(int j =1; j <=n;j++){
+                ans[i][j] = ans[i-1][j] + ans[i][j-1] - ans[i-1][j-1] + arr[i][j];
             }
         }
-        for(int k = 1; k<=m;k++){
+        for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            int ans = arr[x2][y2] - arr[x2][y1-1] - arr[x1-1][y2] + arr[x1-1][y1-1];
-            sb.append(ans +"\n");
+            int answer = ans[x2][y2] - ans[x2][y1-1] - ans[x1-1][y2] + ans[x1-1][y1-1];
+            sb.append(answer + "\n");
         }
         System.out.print(sb);
     }
