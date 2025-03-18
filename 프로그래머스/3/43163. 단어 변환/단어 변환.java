5536692f0d7d42d8a0d41a39507b0033598f984c
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-03-18 21:31:42 +0900

    [level 3] Title: 단어 변환, Time: 0.58 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,41 @@
 import java.util.*;
 class Solution {
-    boolean[] visited;
-    int ans = Integer.MAX_VALUE;
+    
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        visited = new boolean[words.length];
-        if(!Arrays.asList(words).contains(target)){
+        Queue<String> queue = new LinkedList<>();
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        if(!set.contains(target)){
             return 0;
-        }else{
-            dfs(begin, target, 0, words);
-            answer = ans;
         }
-        return answer;
+        
+        queue.offer(begin);
+        set.remove(begin);
+        
+        while(!queue.isEmpty()){
+            for(int i =0; i < queue.size(); i++){
+                String current = queue.poll();
+                if(current.equals(target)){
+                    return answer;
+                }
+                for(String word : set.toArray(new String[set.size()])){
+                    if(canConvert(current,word)){
+                        queue.offer(word);
+                        set.remove(word);
+                    }
+                }
+            }
+            answer++;
+        }
+        return  0;
     }
-    public void dfs(String word, String target, int count, String[] words){
-        if(word.equals(target)){
-            ans = Math.min(count,ans);
-        }
-        for(int i =0; i < words.length; i++){
-            if(visited[i]){
-                continue;
-            }
-            if(change(word,words[i])){
-                visited[i] = true;
-                dfs(words[i],target,count+1,words);
-                visited[i] = false;
+    private boolean canConvert(String word1, String word2){
+        int diffCnt = 0;
+        for(int i =0; i<word1.length(); i++){
+            if(word1.charAt(i) != word2.charAt(i)){
+                diffCnt++;
             }
         }
-        return;
-    }
-    public boolean change(String word,String w){
-        int cnt =0;
-        for(int i =0; i < word.length(); i++){
-            if(word.charAt(i) != w.charAt(i)){
-                cnt++;
-            }
-            if(cnt>1){
-                return false;
-            }
-        }
-        return true;
+        return diffCnt == 1;
     }
 }
