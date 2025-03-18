fe8ec9ac154536639c30f8721b123c44cf06798d
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-03-18 19:29:52 +0900

    [level 2] Title: 타겟 넘버, Time: 75.45 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,26 @@
+import java.util.*;
 class Solution {
-    int answer = 0;
     public int solution(int[] numbers, int target) {
-        dfs(0,numbers,target,0);
-        return answer;
-    }
-    private void dfs(int depth, int[] numbers, int target, int sum){
-        if(depth == numbers.length){
-            if(sum == target){
+        int answer = 0;
+        int start = 0;
+        
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        
+        for(int num : numbers){
+            int len = queue.size();
+            for(int i =0; i < len; i++){
+                int val = queue.remove();
+                queue.add(val + num);
+                queue.add(val - num);
+            }
+        }
+        while(!queue.isEmpty()){
+            int val = queue.remove();
+            if(val == target){
                 answer++;
             }
-            return;
         }
-        dfs(depth+1,numbers,target, sum+numbers[depth]);
-        dfs(depth+1,numbers,target, sum-numbers[depth]);
+        return answer;
     }
 }
