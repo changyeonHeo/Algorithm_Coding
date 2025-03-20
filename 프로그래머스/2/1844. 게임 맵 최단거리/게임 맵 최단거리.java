1a8d4125d8d594a9717148e18f45e00606a4b51a
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-03-20 23:40:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.83 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -24,16 +24,16 @@ class Solution {
             int cX = current[0];
             int cY = current[1];
             
-            for(int i =0; i < 4;i++){
+            for(int i =0; i < 4; i++){
                 int nX = cX + dx[i];
                 int nY = cY + dy[i];
                 
-                if(nX < 0 || nX > maps.length -1 || nY < 0 || nY > maps[0].length-1){
-                    continue;                    
+                if(nX < 0 || nX > maps.length -1 || nY <0 || nY >maps[0].length -1){
+                    continue;
                 }
-                if(visited[nX][nY] == 0 && maps[nX][nY] == 1){
+                if(maps[nX][nY] == 1 && visited[nX][nY] == 0){
                     visited[nX][nY] = visited[cX][cY] + 1;
-                    queue.add(new int[]{nX, nY});
+                    queue.add(new int[]{nX,nY});
                 }
             }
         }
