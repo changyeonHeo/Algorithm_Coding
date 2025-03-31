bd56da71c662bfb01d2583b42aefbb4b297e807e
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-03-31 18:35:19 +0900

    [level 3] Title: 금과 은 운반하기, Time: 127.22 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -4,24 +4,25 @@ class Solution {
         long answer = -1;
         
         long left = 1;
-        long right = (long)(10e9 * 2 * 10e5*2);
+        long right = (long)(10e9 * 2 * 10e5 * 2);
         int len = s.length;
         
-        while(left<= right){
-            long mid = (right + left) / 2;
-            int gold =0, silver = 0, sum = 0;
+        while(left <= right){
+            long mid = (left + right) /2;
+            int gold = 0, silver = 0, sum =0;
             for(int i =0; i < len; i++){
                 int weight = w[i];
                 int time = t[i];
-                long cnt = mid/(time * 2);
-                if((mid%(time*2)) >= time){
+                
+                long cnt = mid / (time * 2);
+                if((mid % (time * 2)) >= time){
                     cnt++;
                 }
-                gold += Math.min(g[i],weight*cnt);
-                silver += Math.min(s[i],weight*cnt);
-                sum += Math.min(g[i]+s[i],weight*cnt);
+                gold += Math.min(g[i],weight * cnt);
+                silver += Math.min(s[i], weight * cnt);
+                sum += Math.min(g[i] + s[i], weight * cnt);
             }
-            if(gold >= a&& silver >= b && sum >=a+b){
+            if(gold >= a && silver >= b && sum >= a + b){
                 answer = mid;
                 right = mid - 1;
             }else{
