b964e061f11f07e034d812f98b29869aeef43869
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-04-30 23:33:54 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 91.61 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,34 +1,31 @@
 import java.util.*;
-
 public class Solution {
-	public static int solution(int[] q1, int[] q2) {
+	public static int solution(int[] queue1, int[] queue2) {
 		int ans = 0;
-		
-		long total=0;
-		long q1Sum=0;
-		Queue<Integer> qq1 = new LinkedList<Integer>();
-		Queue<Integer> qq2 = new LinkedList<Integer>();
-		for(int i=0; i<q1.length; i++) { 
-			total += q1[i]+q2[i];
-			q1Sum += q1[i];
-			qq1.add(q1[i]);
-			qq2.add(q2[i]);
-		}
-		if(total%2!=0) return -1;
-		long target = total/2;
-		while(true) {
-			if(ans>(q1.length+q2.length)*2) return -1;
-			if(q1Sum==target) break;
-			else if(q1Sum>target) {
-				q1Sum-=qq1.peek();
-				qq2.add(qq1.poll());
-			}else {
-				q1Sum += qq2.peek();
-				qq1.add(qq2.poll());
-			}
-			ans++;
-		}
-		
-		return ans;
+        long total = 0;
+        long q1sum = 0;
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        for(int i =0; i < queue1.length; i++){
+            total += ( queue1[i] + queue2[i]);
+            q1sum += queue1[i];
+            q1.add(queue1[i]);
+            q2.add(queue2[i]);
+        }
+        if(total % 2 != 0) return -1;
+        long target = total / 2;
+        while(true){
+            if(ans>(queue1.length + queue2.length) * 2) return -1;
+            if(q1sum == target) break;
+            else if(q1sum > target){
+                q1sum -= q1.peek();
+                q2.add(q1.poll());
+            }else{
+                q1sum += q2.peek();
+                q1.add(q2.poll());
+            }
+            ans++;
+        }
+        return ans;
 	}
 }
