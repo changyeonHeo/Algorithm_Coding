7b543b70d100a37b3bee23ea9d6518f128681ef3
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-05-07 23:21:22 +0900

    [level 3] Title: 여행경로, Time: 99.02 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     int length = 0;
     boolean[] visited;
-    ArrayList<String> list;
+    ArrayList<String> list;    
     public String[] solution(String[][] tickets) {
         String[] answer = {};
         length = tickets.length;
@@ -13,15 +13,15 @@ class Solution {
         answer = list.get(0).split(" ");
         return answer;
     }
-    public void dfs(String start, String route, String[][] tickets, int count){
+    public void dfs(String start, String route,String[][] tickets,int count){
         if(count == length){
             list.add(route);
             return;
         }
-        for(int i =0; i< length; i++){
+        for(int i =0; i < length; i++){
             if(!visited[i] && start.equals(tickets[i][0])){
                 visited[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1],tickets, count+ 1);
+                dfs(tickets[i][1], route+ " " + tickets[i][1], tickets,count+1);
                 visited[i] = false;
             }
         }
