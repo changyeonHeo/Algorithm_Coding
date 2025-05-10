9a97071d5aa34958002dbd5e8837021c02b0e0a2
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-05-10 17:48:54 +0900

    [level 2] Title: 가장 큰 수, Time: 216.66 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -5,12 +5,12 @@ class Solution {
         for(int i =0; i < numbers.length; i++){
             arr[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(arr,(o1,o2) -> (o2+o1).compareTo(o1+o2));
+        Arrays.sort(arr,(o1,o2) ->(o2+o1).compareTo(o1+o2));
         if(arr[0].equals("0")){
             return "0";
         }
         StringBuilder answer = new StringBuilder();
-        for(int i=0; i< arr.length; i++){
+        for(int i =0; i< arr.length; i++){
             answer.append(arr[i]);
         }
         return answer.toString();
