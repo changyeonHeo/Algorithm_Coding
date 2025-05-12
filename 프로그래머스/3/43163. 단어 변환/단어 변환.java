39e8fd84979b4480b0dc4577731f287ce8d8a535
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-05-12 17:01:25 +0900

    [level 3] Title: 단어 변환, Time: 0.72 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -17,7 +17,7 @@ class Solution {
                     return answer;
                 }
                 for(String word : set.toArray(new String[set.size()])){
-                    if(canConvert(current,word)){
+                    if(canConvert(current, word)){
                         queue.offer(word);
                         set.remove(word);
                     }
@@ -29,7 +29,7 @@ class Solution {
     }
     private boolean canConvert(String word1, String word2){
         int diffCnt = 0;
-        for(int i =0; i < word1.length(); i++){
+        for(int i =0; i <word1.length(); i++){
             if(word1.charAt(i) != word2.charAt(i)){
                 diffCnt++;
             }
