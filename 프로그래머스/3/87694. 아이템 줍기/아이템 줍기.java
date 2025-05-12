92d697f6660a8ee1dfc2a15ae6520e4065bd113b
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-05-12 19:59:23 +0900

    [level 3] Title: 아이템 줍기, Time: 1.78 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -7,13 +7,13 @@ class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         map = new int[101][101];
         for(int[] r : rectangle){
-            fill(2*r[0], 2*r[1], 2*r[2], 2*r[3]);
+            fill(2*r[0], 2 * r[1], 2*r[2], 2*r[3]);
         }
-        bfs(2*characterX, 2*characterY, 2*itemX, 2*itemY);
+        bfs(2*characterX, 2*characterY,2*itemX, 2*itemY);
         return answer;
     }
     public void fill(int x1, int y1, int x2, int y2){
-        for(int i =x1; i<= x2; i++){
+        for(int i =x1; i <=x2; i++){
             for(int j =y1; j <= y2; j++){
                 if(map[i][j] == 2){
                     continue;
@@ -35,18 +35,18 @@ class Solution {
             for(int i =0; i < 4; i++){
                 int moveX = move[0] + dx[i];
                 int moveY = move[1] + dy[i];
-                if(validation(moveX,moveY)){
+                if(validation(moveX, moveY)){
                     continue;
                 }
                 if(map[moveX][moveY] == 1 && cost[moveX][moveY] == 0){
-                    cost[moveX][moveY] = cost[move[0]][move[1]] +1;
+                    cost[moveX][moveY] = cost[move[0]][move[1]] + 1;
                     q.offer(new int[]{moveX, moveY});
                 }
             }
         }
-        answer = cost[itemX][itemY]/2;
+        answer = cost[itemX][itemY] / 2;
     }
     public boolean validation(int x, int y){
-        return(0>x || 0 > y || x> 100 || y > 100);
+        return (0>x||0>y||x>100 || y>100);
     }
 }
