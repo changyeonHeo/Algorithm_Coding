bf4a904bf1173c456f301e12f74e0dbb28233fc6
changyeonHeo <150575238+changyeonHeo@users.noreply.github.com>
2025-05-19 18:31:43 +0900

    [level 2] Title: 주식가격, Time: 43.69 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] ans = new int[prices.length];
-        Stack<Integer>stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         
         for(int i =0; i < prices.length; i++){
             while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
@@ -11,7 +11,6 @@ class Solution {
             }
             stack.push(i);
         }
-        
         while(!stack.isEmpty()){
             ans[stack.peek()] = prices.length - stack.peek() - 1;
             stack.pop();
